744d5bddc02a3122eb29d7f47785869f45224155
rebewcosta <webercostag@gmail.com>
2025-05-27 08:48:28 -0300

    tarefas3

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import DashboardPage from './pages/DashboardPage';
 import ClientesPage from './pages/ClientesPage';
 import MeusProcessosPage from './pages/MeusProcessosPage';
 import AgendaPage from './pages/AgendaPage';
-// import TarefasPage from './pages/TarefasPage'; // Comentado para restaurar
+import TarefasPage from './pages/TarefasPage'; 
 import FinanceiroPage from './pages/FinanceiroPage';
 import DocumentosPage from './pages/DocumentosPage';
 import RelatoriosPage from './pages/RelatoriosPage';
@@ -63,7 +63,7 @@ function App() {
           <Route path="/meus-processos" element={<MeusProcessosPage />} />
           <Route path="/clientes" element={<ClientesPage />} />
           <Route path="/agenda" element={<AgendaPage />} />
-          {/* <Route path="/tarefas" element={<TarefasPage />} /> */}{/* Comentado para restaurar */}
+          <Route path="/tarefas" element={<TarefasPage />} />
           <Route path="/financeiro" element={<FinanceiroPage />} />
           <Route path="/documentos" element={<DocumentosPage />} />
           <Route path="/relatorios" element={<RelatoriosPage />} />
