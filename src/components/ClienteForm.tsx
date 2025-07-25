75ede7cd0b03b7b9d11f3150569f40231bae7470
rebewcosta <webercostag@gmail.com>
2025-07-25 20:33:44 -0300

    gemini11

--- src/components/ClienteForm.tsx
+++ src/components/ClienteForm.tsx
@@ -5,7 +5,7 @@ import {
   ClienteFormValidation,
 } from '@/hooks/clientes/clienteValidation'
 import ClienteFormFields from './clientes/ClienteFormFields'
-import ConsultaCep from './correios/ConsultaCep' // Importação default
+import { ConsultaCep } from './correios/ConsultaCep' // A correção está nesta linha
 import { Cliente } from '@/hooks/clientes/types'
 
 interface ClienteFormProps {
